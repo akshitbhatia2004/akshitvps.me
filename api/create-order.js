3fa59ba1b854e013812b0f1b0307f91541f85a1a
Akshit Bhatia <88152268+akshitbhatia2004@users.noreply.github.com>
2025-12-27 17:16:05 +0530

    Update create-order.js

--- api/create-order.js
+++ api/create-order.js
@@ -1,3 +1,5 @@
+import { createOrder } from "../lib/orderStore";
+
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed" });
